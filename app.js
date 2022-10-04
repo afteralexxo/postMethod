095c6977f2ffb31bb743e750448ec5ce9fdf6b21
creative-og <creative | Alex>
2022-10-04 09:05:12 -0700

    steal on working

--- app.js
+++ app.js
@@ -10,14 +10,7 @@ app.use(express.urlencoded({
 
 app.listen(10)
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'alexunder'
-})
-
-let info = { firstname: '', title: '' }
+let info = { title: '' }
 
 app.get('/', (req, res) => {
 
@@ -34,43 +27,39 @@ app.get('/about', (req, res) => {
     info.title = 'about'
     res.render('about', info)
 })
-let insrt = [[info.firstname, info.lastname, info.email, info.password]]
-console.log();
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'alexunder'
+})
+
+con.connect(function (err) {
+    if (err) throw err
+    console.log('connect successfully');
+});
+
+
+function insertData(firstname, lastname, email, password) {
+
+    let sql = 'INSERT INTO alexuser(firstname, lastname, email, password) VALUES ?'
+    con.query(sql, [[[firstname, lastname, email, password]]], (err, result) => {
+        if (err) throw err
+        console.log('successfully inserted');
+    });
+
+}
+
 
 app.post('/user/signup', (req, res) => {
     info.firstname = req.body.firstname
     info.lastname = req.body.lastname
     info.email = req.body.email
     info.password = req.body.password
-    let insrtData = [[info.firstname, info.lastname, info.email, info.password]]
 
-    con.connect(err => {
-        if (err) throw err
-        console.log('db connection checked');
-        con.query('SELECT firstname FROM alexuser', (err, result) => {
-            if (err) throw err
-            console.log( ' selected result  ', result);
-            const sql = 'INSERT INTO alexuser(firstname, lastname, email, password) VALUES ?'
-            con.query(sql, [insrtData], (err) => {
-                if (err) throw err
-                console.log('inserted');
-            })
+    insertData(req.body.firstname, req.body.lastname, req.body.email, req.body.password)
 
-            //     result.forEach(el => {
-            //         if (true) {
-            //             console.log(el , ' ', result);
-            //         }
-            //         else {
-            // const sql = 'INSERT INTO alexuser(firstname, lastname, email, password) VALUES ?'
-            // con.query(sql, [insrtData], (err, result) => {
-            //     if (err) throw err
-            //     console.log('successfully inserted to table alexunder');
-            //             })
-            //         }
-            //     });
+    return res.redirect('/signup')
 
-        })
-        return res.redirect('/signup')
-    }
-    )
 })
